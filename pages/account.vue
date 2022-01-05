a42ddc9bf2567dca8525ce9d701ac5499e0040ef
Alexandre Lri <alexandre.lairie@gmail.com>
2022-01-05 14:23:58 +0100

    add account pages

---
+++ pages/account.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container>
+    <h1>Account</h1>
+    <div class="mt-lg-5">
+      <v-card elevation="2" class="pa-md-4 mx-lg-auto">
+        <v-card-title>Solde</v-card-title>
+        <v-card-text
+          >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa,
+          quam. Magni rerum corrupti officiis officia culpa voluptates debitis
+          cupiditate voluptatibus vero quidem, ducimus magnam, ullam amet in
+          veritatis commodi. Accusantium?</v-card-text
+        >
+      </v-card>
+    </div>
+    <div class="mt-lg-5">
+      <a href="/">
+        <v-btn color="secondary" elevation="2" large
+          ><v-icon>mdi-arrow-left</v-icon>&nbsp; Retour</v-btn
+        >
+      </a>
+    </div>
+  </v-container>
+</template>
+
+
+<style scoped>
+</style>
+
+
+<script>
+export default {}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,19 @@
 <template>
-  <v-app>
-    <v-navigation-drawer v-model="drawer" app>
-      <!--  -->
-    </v-navigation-drawer>
-
-    <v-app-bar app>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Application Deployment</v-toolbar-title>
-    </v-app-bar>
-
-    <v-main>
-      <v-container fluid>
-        <h1>Bonjour !</h1>
-        <p>Test</p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis,
-          inventore ea, nemo provident officia libero atque magni deleniti
-          illum, nisi sit totam eaque dolorum. Quod magni modi tempora rerum
-          quidem!
-        </p>
-        <p>Un autre test après push</p>
-        <br />
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque hic
-          optio esse quidem accusamus aut repellat officia suscipit tempora, eos
-          ipsa numquam culpa explicabo possimus qui animi repellendus eum autem.
-        </p>
-      </v-container>
-    </v-main>
-  </v-app>
+  <v-container>
+    <h1>Homepage</h1>
+    <div class="mt-lg-5">
+      <a href="/account">
+        <v-btn color="secondary" elevation="2" large>Account Page &nbsp; <v-icon>mdi-arrow-right</v-icon></v-btn>
+      </a>
+    </div>
+  </v-container>
 </template>
 
+
+<style scoped>
+</style>
+
+
 <script>
-export default {
-  data: () => ({ drawer: null }),
-}
+export default {}
 </script>
